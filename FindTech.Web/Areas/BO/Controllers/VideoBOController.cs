8cdd147b2cc44022a31dc40853deacdf54780fde
Tho Phan <pvtho.uit@gmail.com>
2015-04-11 13:11:23 +0700

    [Video][BO]Create
    add video

--- FindTech.Web/Areas/BO/Controllers/VideoBOController.cs
+++ FindTech.Web/Areas/BO/Controllers/VideoBOController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using FindTech.Entities.Models;
 using FindTech.Services;
 using FindTech.Web.Areas.BO.Models;
+using Newtonsoft.Json;
 using Repository.Pattern.UnitOfWork;
 using System;
 using System.Collections.Generic;
@@ -30,6 +32,34 @@ namespace FindTech.Web.Areas.BO.Controllers
         {
             var videos = videoService.Query().Select();
             return Json(videos.Select(Mapper.Map<VideoBOModel>), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Create(string models)
+        {
+            List<VideoBOModel> videoBOModels = JsonConvert.DeserializeObject<List<VideoBOModel>>(models);
+            for (var i = 0; i < videoBOModels.Count; i++)
+            {
+                VideoBOModel videoBOModel = videoBOModels.ElementAt(i);
+                Video video = Mapper.Map<Video>(videoBOModel);
+                videoService.Insert(video);
+                unitOfWork.SaveChanges();
+                videoBOModels.RemoveAt(i);
+                videoBOModels.Add(Mapper.Map<VideoBOModel>(video));
+            }
+            return Json(videoBOModels, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(string videoBOModel)
+        {
+            return Json(true);
+        }
+
+        [HttpPost]
+        public ActionResult Destroy(string videoBOModel)
+        {
+            return Json(true);
         }
     }
 }
