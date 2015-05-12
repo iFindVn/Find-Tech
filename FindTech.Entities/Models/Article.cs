ee6d711f98a7a49a2b864fca4d521c5b009cccd3
Tho Phan <pvtho.uit@gmail.com>
2015-05-12 22:25:14 +0700

    [BO][Device]

--- FindTech.Entities/Models/Article.cs
+++ FindTech.Entities/Models/Article.cs
@@ -21,6 +21,7 @@ namespace FindTech.Entities.Models
         public string Avatar { get; set; }
         public string SquareAvatar { get; set; }
         public string RectangleAvatar { get; set; }
+        public string BannerAvatar { get; set; }
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public BoxSize BoxSize { get; set; }
