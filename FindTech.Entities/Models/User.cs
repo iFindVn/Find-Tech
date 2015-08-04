d01cefa190bac89a175375298142832f86fbdadb
Tho Phan <pvtho.uit@gmail.com>
2015-08-04 23:00:12 +0700

    [Model][User]

---
+++ FindTech.Entities/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using Repository.Pattern.Ef6;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTech.Entities.Models
+{
+    class User:Entity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public string Email { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
